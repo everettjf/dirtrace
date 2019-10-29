c1a1419767ae8140abfe442de587dc2e53482e7f
everettjf <everettjf@live.com>
2019-10-30 00:58:53 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 
 # how to publish python package
 # https://packaging.python.org/tutorials/packaging-projects/#uploading-your-project-to-pypi
+# pip install --user --upgrade setuptools wheel
 # pip install --user --upgrade twine
+
+rm -rf dist
+
+python setup.py sdist bdist_wheel
 
 twine upload --repository-url https://upload.pypi.org/legacy/ dist/* --verbose
