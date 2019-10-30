7c0ebe5ddc42cb1e48e4847b2682ba2808645803
everettjf <everettjf@live.com>
2019-10-30 23:07:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="dirtrace",
-    version="0.0.3",
+    version="0.0.5",
     description="generate chromium's trace event format json file from directory content file size",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/everettjf/dirtrace",
     author="everettjf",
     author_email="everettjf@live.com",
@@ -15,5 +20,5 @@ setup(
     """,
     install_requires=[
     ],
-    zip_safe=False
+    zip_safe=False,
 )
